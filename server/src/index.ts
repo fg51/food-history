d12639536c7e5d72577f708f9ef4fdb75be6e636
kflange <zknsftk1msrk@gmail.com>
2019-10-07 18:05:50 +0900

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,36 @@
-import {ulid} from "ulid";
-import {GraphQLServer} from "graphql-yoga";
-import {QueryResolvers, Resolvers } from '../gen/graphql-resolver-types';
-import {Food} from '../gen/graphql-resolver-types';
+import { ulid } from 'ulid';
+import { GraphQLServer } from 'graphql-yoga';
+import { QueryResolvers, Resolvers } from '../gen/graphql-resolver-types';
+import { Food } from '../gen/graphql-resolver-types';
 
-const values :Food[] =[
-  {
-  id: ulid(),
-  name: "apple",
-},
+const values: Food[] = [
   {
     id: ulid(),
-    name: "banana",
+    name: 'apple'
+  },
+  {
+    id: ulid(),
+    name: 'banana'
   }
-]
+];
 
 const Query: QueryResolvers = {
-  async foods(_parent, _args, _context, _info) {
-  const foods = await values;
-  return foods || null;
-  },
-}
-
-const resolvers: Resolvers = {
-  Query: Query,
-}
-
-const main = async () => {
-  const server = new GraphQLServer({
-    typeDefs: './graphql/schema.graphql',
-    resolvers: resolvers as any,
-  });
-  server.start({port: 8080}, () =>
-    console.log("Server is unning on localhost:8080"),
-  )
+  // async foods(_parent, _args, _context, _info) {
+  async foods() {
+    const foods = await values;
+    return foods || null;
+  }
 };
 
-main()
+const resolvers: Resolvers = {
+  Query: Query
+};
 
+const server = new GraphQLServer({
+  typeDefs: './graphql/schema.graphql',
+  resolvers: resolvers as any // eslint-disable-line
+});
+
+server.start({ port: 8080 }, () =>
+  console.log('Server is unning on localhost:8080')
+);
